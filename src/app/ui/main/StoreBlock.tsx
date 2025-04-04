b503aec17bc1ca01ff975b1bd65a16cc10032637
Pavel Tarakanov <pavel191094@icloud.com>
2025-04-04 21:15:59 +0300

    add StoreBlock comp

---
+++ src/app/ui/main/StoreBlock.tsx
@@ -0,0 +1,52 @@
+import styles from '../../styles/store-block.module.css';
+import background from "../../../../public/salt-body-scrub-1140w.png"
+import wbLogo from "../../../../public/wildberries-logo.svg"
+
+import Image from 'next/image';
+
+export const StoreBlock = () => {
+    return (
+        <div className={styles.wrapper}>
+            <Image
+                className={styles.image}
+                alt="salt body scrab"
+                src={background}
+            />
+            <div className={styles.content}>
+                <h2 className={styles.h2}>
+                    закажите сегодня
+                </h2>
+                <p className={styles.p}>
+                    Подарите себе или своим близким возможность насладиться уходом за телом на высшем уровне. 
+                </p>
+                <div className={styles.aWrapper}>
+                    <a className={styles.a}>
+                        в магазин
+                    </a>
+                </div>
+            </div>
+            
+            <div className={styles.partners}>
+                <Image 
+                    className={styles.wbLogo}
+                    priority
+                    src={wbLogo}
+                    alt="Wildberries logo"
+                />
+                {/* <Image 
+                    className={styles.wbLogo}
+                    priority
+                    src={wbLogo}
+                    alt="Wildberries logo"
+                />
+                <Image 
+                    className={styles.wbLogo}
+                    priority
+                    src={wbLogo}
+                    alt="Wildberries logo"
+                /> */}
+
+            </div>
+        </div>
+    );
+};
