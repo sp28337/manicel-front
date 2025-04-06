751d9e1fbb4b9a06cd0d155e6e74a30786662d65
Pavel Tarakanov <pavel191094@icloud.com>
2025-04-06 18:37:35 +0300

    add Body comp

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,10 +1,12 @@
 import { HeaderCatalog } from "../ui/catalog/HeaderCatalog";
+import { BodyCatalog } from "../ui/catalog/BodyCatalog";
 
 export default function Catalog () {
     return (
         <>
             <div>
                 <HeaderCatalog />
+                <BodyCatalog />
             </div>
         </>
     );
