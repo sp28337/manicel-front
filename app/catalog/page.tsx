25750e4a95777a4e1082095149eebaac24d903db
Pavel Tarakanov <pavel191094@icloud.com>
2025-05-05 15:33:12 +0300

    set up catalog skeletons

--- app/catalog/page.tsx
+++ app/catalog/page.tsx
@@ -1,8 +1,13 @@
-import { HeaderCatalog } from "../ui/catalog/header";
-import { BodyCatalog } from "../ui/catalog/body";
-import styles from "../styles/catalog.module.css";
-import { fetchCatalogProducts } from "../lib/data";
-import { SearchList } from "../ui/search-list";
+import { HeaderCatalog } from "../ui/catalog/header"
+import { BodyCatalog } from "../ui/catalog/body"
+import styles from "../styles/catalog.module.css"
+import { fetchCatalogProducts } from "../lib/data"
+import { SearchList } from "../ui/search-list"
+import { Suspense } from "react"
+import { 
+    CatalogHeaderSkeleton,
+    CatalogBodySkeleton
+} from "../ui/skeletons"
 
 export default async function Page (props: {searchParams?: Promise<{query?: string}>}) {
 
@@ -16,8 +21,12 @@ export default async function Page (props: {searchParams?: Promise<{query?: stri
     return (
         <div className={styles.catalogWrapper}>
             <SearchList query={query} />
-            <HeaderCatalog />
-            <BodyCatalog products={products}/>
+            <Suspense fallback={<CatalogHeaderSkeleton />}>
+                <HeaderCatalog />
+            </Suspense>
+            <Suspense fallback={<CatalogBodySkeleton />}>
+                <BodyCatalog products={products}/>
+            </Suspense>
         </div>
     );
 }
