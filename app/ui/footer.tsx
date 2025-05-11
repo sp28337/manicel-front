b9f66930125132b6ed9a0a62317854fc5c1466c9
Pavel Tarakanov <pavel191094@icloud.com>
2025-05-11 08:13:04 +0300

    footer improvements

--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -14,22 +14,23 @@ export const Footer = () => {
                     <h3 className={styles.h3}>навигация</h3>
                     <div className={styles.linksWrapper}>
                         <Link className={styles.footerLink} href="">о нас</Link>
-                        <Link className={styles.footerLink} href="">каталог</Link>
-                        <Link className={styles.footerLink} href="">доставка и оплата</Link>
-                        <Link className={styles.footerLink} href="">где купить</Link>
+                        <Link className={styles.footerLink} href="/catalog">каталог</Link>
+                        <a className={styles.footerLink} href="https://www.wildberries.ru/brands/310747490-manicel">в магазин</a>
                     </div>
                 </div>
                 <div id="contacts" className={styles.contactsWrapper}>
                     <h3 className={styles.h3}>контакты</h3>
                     <div className={styles.linksWrapper}>
-                        <a href="">+7 (999) 999-99-99</a>
-                        <a className={styles.email} href="">example@example.com</a>
-                        <address className={styles.address}>г. Москва, ул. Пушкина, д. 10</address>
+                        <a href="">+7-(920)-941-53-89</a>
+                        <a className={styles.email} href="">rejx.nikita@bk.ru</a>
+                        <address className={styles.address}>г. Вязники, ул Сенькова, д 9/8</address>
                     </div>
                 </div>
             </div>
             <div className={styles.contactUsButtonWrapper}>
-                <a className={styles.contactUsButton}>
+                <a className={styles.contactUsButton}
+                    href="mailto:rejx.nikita@bk.ru"
+                >
                     связаться с нами
                 </a>
             </div>
