4eed0f8e09f5628a5cac4f252c0fd0814135ea78
Pavel Tarakanov <pavel191094@icloud.com>
2025-05-13 15:46:13 +0300

    header improvements

--- app/ui/header.tsx
+++ app/ui/header.tsx
@@ -1,8 +1,8 @@
 import commonStyles from "../styles/common.module.css"
-import headerStyles from "../styles/header.module.css"
+import styles from "../styles/header.module.css"
 import Image from "next/image"
-import allPng from "@/public/all-800w.png"
-import background from "@/public/salt-body-scrub-500w.png"
+import mainImg from "@/public/all-800w.png"
+import backgroundImg from "@/public/salt-body-scrub-500w.png"
 import { 
     EcoFriendlySVG, 
     WithLoveSVG, 
@@ -14,55 +14,57 @@ export const Header = () => {
 
     return (
         <>
-                <header className={headerStyles.headerWrapper}>
-                    <Image
-                        className={headerStyles.image}
-                        src={background}
-                        alt="pretty salt body scrab text on background"
-                        quality={0}
-                        sizes="100vw"
-                        priority
-                    />
-                    <div className={`${commonStyles.container} ${headerStyles.headerBody}` }>
-                        
-                        <div className={headerStyles.textHeader}>
-                            <h1 className={headerStyles.h1Header}>НАСТОЯЩИЙ СПА-РИТУАЛ<br />
-                                <span className={headerStyles.spanHeader}>ОБНОВЛЕНИЯ<br/> И СИЯНИЯ</span>
-                            </h1>
-                            <p className={headerStyles.pHeader}>Познакомьтесь с нашими натуральными солевыми скрабами, доступными в разнообразии ароматов, которые подарят вашей коже мягкость и гладкость, словно после спа-процедур!</p>
-                            <div className={headerStyles.icons}>
-                                <div className={headerStyles.iconWrapper}>
-                                    <EcoFriendlySVG  />
-                                    <span className={headerStyles.iconDescribtion}>
-                                        eco friendly
-                                    </span>
-                                </div>
-                                <div className={headerStyles.iconWrapper}>
-                                    <WithLoveSVG  />
-                                    <span className={headerStyles.iconDescribtion}>
-                                        made with love
-                                    </span>
-                                </div>
-                                <div className={headerStyles.iconWrapper}>
-                                    <NoAnimalTestingSVG  />
-                                    <span className={headerStyles.iconDescribtion}>
-                                        cruelty free
-                                    </span>
-                                </div>
+            <header className={styles.headerWrapper}>
+                <Image
+                    className={styles.backgroundImage}
+                    src={backgroundImg}
+                    alt="pretty salt body scrab text on background"
+                    quality={0}
+                    sizes="100vw"
+                    priority
+                />
+                <div className={`${commonStyles.container} ${styles.header}` }>
+                    
+                    <div className={styles.textBlock}>
+                        <h1 className={styles.h1}>НАСТОЯЩИЙ СПА-РИТУАЛ<br />
+                            <span className={styles.span}>ОБНОВЛЕНИЯ<br/> И СИЯНИЯ</span>
+                        </h1>
+                        <p className={styles.p}>Познакомьтесь с нашими натуральными солевыми скрабами, доступными в разнообразии ароматов, которые подарят вашей коже мягкость и гладкость, словно после спа-процедур!</p>
+                        <div className={styles.icons}>
+                            <div className={styles.iconWrapper}>
+                                <EcoFriendlySVG  />
+                                <span className={styles.iconDescribtion}>
+                                    eco friendly
+                                </span>
+                            </div>
+                            <div className={styles.iconWrapper}>
+                                <WithLoveSVG  />
+                                <span className={styles.iconDescribtion}>
+                                    made with love
+                                </span>
+                            </div>
+                            <div className={styles.iconWrapper}>
+                                <NoAnimalTestingSVG  />
+                                <span className={styles.iconDescribtion}>
+                                    cruelty free
+                                </span>
                             </div>
                         </div>
-                        <div className={headerStyles.imgWrapper}>
-                            <Image className={headerStyles.imgAll}
-                                src={allPng} 
-                                alt="piramyd of all scrubs with raspberry on top of some cans"
-                                quality={100}
-                                sizes="100vw"
-                                priority
-                            />
-                        </div>
                     </div>
-                </header >
-            <WaveSVG />
+                    <div className={styles.imgWrapper}>
+                        <Image className={styles.mainImage}
+                            src={mainImg} 
+                            alt="piramyd of all scrubs with raspberry on top of some cans"
+                            quality={100}
+                            sizes="100vw"
+                            priority
+                        />
+                    </div>
+                </div>
+                <div className={styles.wave}>
+                    <WaveSVG />
+                </div>
+            </header >
         </>
     )
 }
