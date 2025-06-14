e5df8483b98e1ab7d6d16c6fbfc40eff17dfb496
Pavel Tarakanov <pavel191094@icloud.com>
2025-06-14 16:56:18 +0300

    style fixes for text-block

--- app/ui/homepage/text-block.tsx
+++ app/ui/homepage/text-block.tsx
@@ -18,19 +18,25 @@ export const TextBlock = () => {
             <div className={styles.blocks}>
                 <section className={`${styles.blockWrapper} ${styles.block1}`}>
                     <h3 className={styles.h3}>БЕЗОПАСНОСТЬ</h3>
-                    <p className={styles.p}>Все продукты MANICEL проходят строгий контроль качества, гарантируя безопасность и эффективность каждого скраба</p>        
+                    <span className={styles.pBackground}>
+                        <p className={styles.p}>Все продукты MANICEL проходят строгий контроль качества, гарантируя безопасность и эффективность каждого скраба</p>
+                    </span>
                 </section>
                 <section className={`${styles.blockWrapper} ${styles.block2}`}>
                     <h3 className={styles.h3}>ЭФФЕКТИВНОСТЬ</h3>
-                    <p className={styles.p}>наши скрабы глубоко очищают, увлажняют и питают кожу, оставляя её мягкой и бархатистой</p>        
+                    <span className={styles.pBackground}>
+                        <p className={styles.p}>Наши скрабы глубоко очищают, увлажняют и питают кожу, оставляя её мягкой и бархатистой</p>        
+                    </span>
                 </section>
                 <section className={`${styles.blockWrapper} ${styles.block3}`}>
                     <h3 className={styles.h3}>КАЧЕСТВО</h3>
-                    <p className={styles.p}>MANICEL использует только лучшие ингредиенты, чтобы вы могли наслаждаться превосходным результатом</p>        
+                    <span className={styles.pBackground}>
+                        <p className={styles.p}>MANICEL использует только лучшие ингредиенты, чтобы вы могли насладиться превосходным результатом</p>        
+                    </span>
                 </section>
             </div>
-            <div className={styles.aboutUsButtonWrapper}>
-                <Link className={styles.aboutUsButton} href="/catalog">
+            <div className={styles.aboutUsButton}>
+                <Link href="/catalog">
                     каталог
                 </Link>
             </div>
