6eee3c3c71ef774bdb7a470fd550c95f5198c15f
Pavel Tarakanov <pavel191094@icloud.com>
2025-06-15 10:03:21 +0300

    styles refactorinfg for homepage

--- app/ui/homepage/text-block.tsx
+++ app/ui/homepage/text-block.tsx
@@ -1,5 +1,4 @@
 import styles from "../../styles/homepage/text-block.module.css"
-import Link from "next/link"
 import { TextBlockLogoSVG } from "../vectors"
 
 export const TextBlock = () => {
@@ -9,9 +8,9 @@ export const TextBlock = () => {
             <div className={styles.textHeaderWrapper}>
                 <h2 className={styles.h2}>
                     что ты получишь,<br/>выбрав
-                <div className={styles.logoSvgWrapper}>
-                    <TextBlockLogoSVG />
-                </div>
+                    <div className={styles.logoSvgWrapper}>
+                        <TextBlockLogoSVG />
+                    </div>
                 </h2>                
             </div>
 
@@ -19,25 +18,31 @@ export const TextBlock = () => {
                 <section className={`${styles.blockWrapper} ${styles.block1}`}>
                     <h3 className={styles.h3}>БЕЗОПАСНОСТЬ</h3>
                     <span className={styles.pBackground}>
-                        <p className={styles.p}>Все продукты MANICEL проходят строгий контроль качества, гарантируя безопасность и эффективность каждого скраба</p>
+                        <p className={styles.p}>
+                            Все продукты MANICEL проходят строгий контроль качества, 
+                            гарантируя безопасность и эффективность каждого скраба
+                        </p>
                     </span>
                 </section>
                 <section className={`${styles.blockWrapper} ${styles.block2}`}>
                     <h3 className={styles.h3}>ЭФФЕКТИВНОСТЬ</h3>
                     <span className={styles.pBackground}>
-                        <p className={styles.p}>Наши скрабы глубоко очищают, увлажняют и питают кожу, оставляя её мягкой и бархатистой</p>        
+                        <p className={styles.p}>
+                            Наши скрабы глубоко очищают, увлажняют и питают кожу, 
+                            оставляя её мягкой и бархатистой
+                        </p>        
                     </span>
                 </section>
                 <section className={`${styles.blockWrapper} ${styles.block3}`}>
                     <h3 className={styles.h3}>КАЧЕСТВО</h3>
                     <span className={styles.pBackground}>
-                        <p className={styles.p}>MANICEL использует только лучшие ингредиенты, чтобы вы могли насладиться превосходным результатом</p>        
+                        <p className={styles.p}>
+                            MANICEL использует только лучшие ингредиенты, 
+                            чтобы вы могли насладиться превосходным результатом
+                        </p>        
                     </span>
                 </section>
             </div>
-            <Link href="/catalog" className={styles.aboutUsButton}>
-                каталог
-            </Link>
         </article>
     )
 }
